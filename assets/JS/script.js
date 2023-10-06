c932667b9249f039208fd7c7c3fde46901aab683
Emmanuel Quaynor <141752821+equaynor@users.noreply.github.com>
2023-10-06 08:32:09 +0000

    Refactor js and uncomment questions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -34,72 +34,72 @@ const quizData = [
     correct: "Satoshi Nakamoto",
     answeredCorrectly: false,
   },
-  // {
-  //     question: "What is the primary purpose of a blockchain?",
-  //     options: ["Sending emails", "Decentralized finance", "Storing data", "Recording transactions"],
-  //     correct: "Recording transactions",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "What is the name of the consensus algorithm used by Bitcoin?",
-  //     options: ["Proof of Stake (PoS)", "Delegated Proof of Stake (DPoS)", "Proof of Work (PoW)", "Proof of Authority (PoA)"],
-  //     correct: "Proof of Work (PoW)",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "What is the term for the process of converting a cryptocurrency into traditional currency?",
-  //     options: ["Mining", "Trading", "Staking", "Fiat conversion"],
-  //     correct: "Fiat conversion",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "Which cryptocurrency is often referred to as 'digital silver'?",
-  //     options: ["Bitcoin", "Ethereum", "Ripple", "Litecoin"],
-  //     correct: "Litecoin",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "What is a 'smart contract' in blockchain technology?",
-  //     options: ["A legally binding document", "A self-executing contract with the terms of the agreement directly written into code", "A contract signed electronically", "A contract between two AI entities"],
-  //     correct: "A self-executing contract with the terms of the agreement directly written into code",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "What does the term 'HODL' mean in the context of cryptocurrencies?",
-  //     options: ["Hold on for dear life", "Hold on and don't let go", "Hope our Dollars leap", "Hugging our digital ledger"],
-  //     correct: "Hold on for dear life",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "Which cryptocurrency is known for its focus on privacy and anonymity?",
-  //     options: ["Bitcoin", "Ethereum", "Monero", "Cardano"],
-  //     correct: "Monero",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "What is the name of the first widely-adopted cryptocurrency?",
-  //     options: ["Ripple", "Ethereum", "Bitcoin", "Litecoin"],
-  //     correct: "Bitcoin",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "In cryptocurrency, what is 'FOMO'?",
-  //     options: ["Fear of making an investment", "Fear of market crashes", "Fear of missing out", "Fear of manipulation"],
-  //     correct: "Fear of missing out",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "Which cryptocurrency was created as a 'joke' or parody of Bitcoin?",
-  //     options: ["Ripple", "Ethereum", "Bitcoin Cash", "Dogecoin"],
-  //     correct: "Dogecoin",
-  //     answeredCorrectly: false
-  // },
-  // {
-  //     question: "What is the term for the process of verifying transactions on a blockchain network and adding them to the public ledger?",
-  //     options: ["Staking", "Mining", "Trading", "Hodling"],
-  //     correct: "Mining",
-  //     answeredCorrectly: false
-  // }
+  {
+      question: "What is the primary purpose of a blockchain?",
+      options: ["Sending emails", "Decentralized finance", "Storing data", "Recording transactions"],
+      correct: "Recording transactions",
+      answeredCorrectly: false
+  },
+  {
+      question: "What is the name of the consensus algorithm used by Bitcoin?",
+      options: ["Proof of Stake (PoS)", "Delegated Proof of Stake (DPoS)", "Proof of Work (PoW)", "Proof of Authority (PoA)"],
+      correct: "Proof of Work (PoW)",
+      answeredCorrectly: false
+  },
+  {
+      question: "What is the term for the process of converting a cryptocurrency into traditional currency?",
+      options: ["Mining", "Trading", "Staking", "Fiat conversion"],
+      correct: "Fiat conversion",
+      answeredCorrectly: false
+  },
+  {
+      question: "Which cryptocurrency is often referred to as 'digital silver'?",
+      options: ["Bitcoin", "Ethereum", "Ripple", "Litecoin"],
+      correct: "Litecoin",
+      answeredCorrectly: false
+  },
+  {
+      question: "What is a 'smart contract' in blockchain technology?",
+      options: ["A legally binding document", "A self-executing contract with the terms of the agreement directly written into code", "A contract signed electronically", "A contract between two AI entities"],
+      correct: "A self-executing contract with the terms of the agreement directly written into code",
+      answeredCorrectly: false
+  },
+  {
+      question: "What does the term 'HODL' mean in the context of cryptocurrencies?",
+      options: ["Hold on for dear life", "Hold on and don't let go", "Hope our Dollars leap", "Hugging our digital ledger"],
+      correct: "Hold on for dear life",
+      answeredCorrectly: false
+  },
+  {
+      question: "Which cryptocurrency is known for its focus on privacy and anonymity?",
+      options: ["Bitcoin", "Ethereum", "Monero", "Cardano"],
+      correct: "Monero",
+      answeredCorrectly: false
+  },
+  {
+      question: "What is the name of the first widely-adopted cryptocurrency?",
+      options: ["Ripple", "Ethereum", "Bitcoin", "Litecoin"],
+      correct: "Bitcoin",
+      answeredCorrectly: false
+  },
+  {
+      question: "In cryptocurrency, what is 'FOMO'?",
+      options: ["Fear of making an investment", "Fear of market crashes", "Fear of missing out", "Fear of manipulation"],
+      correct: "Fear of missing out",
+      answeredCorrectly: false
+  },
+  {
+      question: "Which cryptocurrency was created as a 'joke' or parody of Bitcoin?",
+      options: ["Ripple", "Ethereum", "Bitcoin Cash", "Dogecoin"],
+      correct: "Dogecoin",
+      answeredCorrectly: false
+  },
+  {
+      question: "What is the term for the process of verifying transactions on a blockchain network and adding them to the public ledger?",
+      options: ["Staking", "Mining", "Trading", "Hodling"],
+      correct: "Mining",
+      answeredCorrectly: false
+  }
 ];
 
 // Track the indices of questions that have been asked
@@ -194,6 +194,15 @@ function selectOption(selectedOption) {
   nextButton.disabled = false;
 }
 
+// Function to calculate and display the user's score
+function displayScore() {
+  questionText.textContent = `Your Score: ${score}/${quizData.length}`;
+  optionsList.innerHTML = "";
+  nextButton.style.display = "none";
+  stopButton.style.display = "inline-block";
+  restartButton.style.display = "inline-block";
+}
+
 // Event listener for the "Next" button
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
@@ -204,15 +213,6 @@ nextButton.addEventListener("click", () => {
     displayScore(); // Display the user's score
   }
 });
-
-// Function to calculate and display the user's score
-function displayScore() {
-  questionText.textContent = `Your Score: ${score}/${quizData.length}`;
-  optionsList.innerHTML = "";
-  nextButton.style.display = "none";
-  stopButton.style.display = "inline-block";
-  restartButton.style.display = "inline-block";
-}
 
 // Event listener for the "Stop Quiz" button
 stopButton.addEventListener("click", () => {
